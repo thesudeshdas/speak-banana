d2298c98e531876c7e462c162822a3dde3c3a037
thesudeshdas <sudeshkumardas7@gmail.com>
2020-11-30 12:31:51 +0530

    upate: cleaned js code

--- app.js
+++ app.js
@@ -1,62 +1,28 @@
-// get the elements to work on
 const textInput = document.querySelector('#text-area-input');
 const textOutput = document.querySelector('#text-area-output');
 const btnTranslate = document.querySelector('#btn-translate');
 
-
-
 let url = 'https://api.funtranslations.com/translate/minion.json';
 
-
-
-
-
-
-const getUrl = text => {
-    return url + '?text=' + text;
-}
-
-
-
+const getUrl = text => { return url + '?text=' + text; };
 
 const showError = error => {
     alert('Uh Oh! Some error occured\n\n' + error + '\n\nPlease try after sometime.');
     console.log({error});
-}
-
-
-// function to translate
-
-
+};
 
 const translate = () => {
-    
     const text = textInput.value;
-    
-    
     
     fetch(getUrl(text))
         .then(response => response.json())
         .then(json => showOutput(json))
         .catch(showError)
-    
-    
-    
-}
+};
 
 const showOutput = (text) => {
     console.log(text)
     textOutput.innerHTML = text.contents.translated;
-}
+};
 
-
-
-
-
-
-
-
-
-
-// event listening
 btnTranslate.addEventListener('click', translate);
